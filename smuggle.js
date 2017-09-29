695b811f978b8058fcb7fad9aa08b8efc421bf29
Keith Petrino <kepe@kepe.org>
2017-09-29 01:04:25 -0400

    automatic numerical type conversion; simplified code

--- smuggle.js
+++ smuggle.js
@@ -1,40 +1,33 @@
 var smuggle = {
 
-  parse: function (sming) { // sming is the input string
+  parse: function (sming, numify = true) { // sming is the input string; numify for type conversion
 
-    if (!/string/i.test(typeof sming)) {
-      throw "Argument must be of type 'string'.";
-    }
+    /string/i.test(typeof sming) || throw "Argument must be of type 'string'.";
     sming = sming.match(/.*?(^|[^`])(``)*(,,|;;|$)/g);
-    if (!sming) {
-      throw "Input string must conform to 'smuggle' syntax (see documentation).";
-    }
+    sming || throw "Input string must conform to 'smuggle' syntax (see documentation).";
     var smob = {}; // the object that will be returned at the end
     sming.forEach(function (item) {
-      // strip out those trailing delimeters
+      // strip trailing delimeters
       item = item.replace(/(.*(^|[^`])(``)*)(,,|;;)$/,'$1');
       // separate key and value
       item = item.match(/(.*(?:^|[^`])(?:``)*)::(.*)/);
-      if (!item) {
-        throw "Input string must conform to 'smuggle' syntax (see documentation).";
-      }
-      // populate object; strip escape characters
-      smob[item[1].replace(/`(.|$)/g, '$1')] = item[2].replace(/`(.|$)/g, '$1');
+      item || throw "Input string must conform to 'smuggle' syntax (see documentation).";
+      // populate object; strip escape characters; numerical type conversion
+      var k = item[1].replace(/`(.|$)/g, '$1');
+      var v = item[2].replace(/`(.|$)/g, '$1');
+      /^[0-9.]+$/.test(v) && numify && (v = +v);
+      smob[k] = v;
     });
     return smob;
   },
 
   stringify: function (smob, del = ',,') { // smob is the object to be stringified
 
-    if (!/object/i.test(typeof smob)) {
-      throw "Argument must be of type 'object'.";
-    }
-    if (!/,,|;;/.test(del)) {
-      throw "The delimeter must be one of: ',,' or ';;'.";
-    }
+    /object/i.test(typeof smob) || throw "Argument must be of type 'object'.";
+    /,,|;;/.test(del) || throw "The delimeter must be one of: ',,' or ';;'.";
     var sming = ''; // the string that will be returned at the end
     for (var k in smob) {
-      !sming || (sming += del);
+      sming && (sming += del);
       sming += k.replace(/(,,|;;|::|`)/g, '`$1') + '::' + smob[k].replace(/(,,|;;|::|`)/g, '`$1');
     }
     return sming;
